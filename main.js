4b29fe450aedd6cfac9131863825a1558e13ede0
Ori Ben Amram <benamram.ori@gmail.com>
2023-03-27 14:00:46 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function onInit() {
+    const strHTML = `
+        <h1>Wow, welcome!</h1> 
+        <img src="imgs/rocks.avif" alt="Rocks image">
+        `
+    document.querySelector('.content').innerHTML = strHTML
+}
